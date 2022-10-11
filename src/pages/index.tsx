fcacc44baf1a5f103b93700bed4385ed55092913
Christian <pleasefloyd73@gmail.com>
2022-10-11 14:38:01 -0300

    make my own starter config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,20 @@
-export default function Home() {
-  return <div></div>
+import { Flex, Heading, Text } from "@chakra-ui/react"
+import Head from 'next/head'
+import DarkModeSwitch from './../components/layout/DarkModeSwitcher/index'
+
+const Home = () => {
+  return (
+    <>
+      <Flex
+        alignItems='center'
+        justifyContent='center'
+        h='100vh'
+      >
+        <DarkModeSwitch />
+      </Flex>
+    </>
+  )
+
 }
+
+export default Home
